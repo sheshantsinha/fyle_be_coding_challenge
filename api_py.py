446c0764102e7048947153527cc6b2ca235e3a3e
sheshantsinha <32589970+sheshantsinha@users.noreply.github.com>
2019-10-09 11:21:30 +0530

    Add files via upload

---
+++ api_py.py
@@ -0,0 +1,121 @@
+#url http://172.105.39.232:5000/api/
+
+from flask import Flask,request,jsonify
+import psycopg2
+app=Flask(__name__)
+
+max_len=127857
+col=('ifsc','bank_id','city','state','branch','district','address')
+def connect():
+	connection = psycopg2.connect(user = "postgres",
+								password = "hello",
+								host = "localhost",
+								port = "5432",
+								database = "postgres")
+
+	cursor = connection.cursor()
+	return connection,cursor
+
+def on_exception():
+	result={'title':"Visit any of the two url",'url1':'http://172.105.39.232:5000/api/branches/autocomplete?q=<>','url2':'http://172.105.39.232:5000/api/branches?q=<>'}
+	return result
+'''
+@Param q:string,limit:integer,offset:integer
+function query_auto will return all the possible matches across branch column only
+@return Array of dictionaries
+
+'''
+
+def query_auto(q,limit,offset):
+	results=[]
+	connection,cursor=connect()
+
+	postgres_insert_query = 'SELECT * FROM indian_banks WHERE branch ILIKE (%s) order by ifsc asc LIMIT (%s) '
+
+	pattern='%{}%'.format(q)
+	cursor.execute(postgres_insert_query,(pattern,limit+offset))
+	res=cursor.fetchall()
+
+	cursor.close()
+	connection.close()
+
+	for row in res[offset:]:
+		results.append(dict(zip(col,row)))
+	return results
+
+'''
+@Param q:string,limit:integer,offset:integer
+function search_all_query will return all the possible matches across branch column only
+@return Array of dictionaries
+
+'''
+
+def search_all_query(q,limit,offset):
+	
+	results=[]
+	connection,cursor=connect()
+
+	postgres_insert_query = 'SELECT * FROM indian_banks WHERE branch ILIKE (%s) or ifsc ILIKE (%s) or city ILIKE (%s) or state ILIKE (%s) or district ILIKE (%s) or address ILIKE (%s)  order by ifsc asc LIMIT (%s) '
+
+	pattern='%{}%'.format(q)
+	temp=(pattern,pattern,pattern,pattern,pattern,pattern,limit+offset)
+	cursor.execute(postgres_insert_query,temp)
+	res=cursor.fetchall()
+
+	cursor.close()
+	connection.close()
+
+	for row in res[offset:]:
+		results.append(dict(zip(col,row)))
+	
+	return results
+	
+
+
+@app.route('/api/branches/autocomplete')
+def autocomplete():
+	try:
+		limit=int(request.args.get('limit'))
+	except Exception as e:
+		limit=max_len
+
+	try:
+		offset=int(request.args.get('offset'))
+	except Exception as e:
+		offset=0
+	query=request.args.get('q')
+
+	if not query:
+		return jsonify({'error':'No query Found'})
+	return jsonify({'branches':query_auto(query,limit,offset)})
+
+@app.route('/api/branches')
+def search_all():
+	try:
+		limit=int(request.args.get('limit'))
+	except Exception as e:
+		limit=max_len
+
+	try:
+		offset=int(request.args.get('offset'))
+	except Exception as e:
+		offset=0
+
+	query=request.args.get('q')
+
+	if not query:
+		return jsonify({'error':'No query Found'})
+
+	return jsonify({'branches':search_all_query(query,limit,offset)})
+
+@app.route('/')
+def landing_url():
+	return jsonify(on_exception())
+
+@app.route('/api')
+def landing_url_api():
+	return jsonify(on_exception())
+	
+if __name__=="__main__":
+	app.run(host='0.0.0.0')
+	
---
+++ bank_detail.sh
@@ -0,0 +1,2 @@
+curl 'http://172.105.39.232:5000/api/branches/autocomplete?q=RTGS&limit=4&offset=0'
+curl 'http://172.105.39.232:5000/api/branches?q=Bangalore&limit=4&offset=0'
